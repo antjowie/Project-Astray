c10b77ba07c6f637b7b4be39db01a31a7b48436f
Angelo <7332321+antjowie@users.noreply.github.com>
2019-12-29 01:40:44 +0100

    Ship can now move up and down

--- Assets/Components/ShipMovement.cs
+++ Assets/Components/ShipMovement.cs
@@ -2,12 +2,14 @@
 
 public class ShipMovement : MonoBehaviour
 {
-    private Vector3 targetRotation;
+    [SerializeField] private float mouseXSensitivity = 1;
+    [SerializeField] private float mouseYSensitivity = 1;
 
-    [SerializeField] private float horizontalSensitivity = 1;
-    [SerializeField] private float verticalSensitivity = 1;
+    [SerializeField] private float forwardSpeed = 50;
+    [SerializeField] private float verticalSpeed = 25;
     [SerializeField] private float horizontalSpeed = 50;
-    [SerializeField] private float verticalSpeed = 50;
+    
+    private Vector3 targetRotation;
 
     private void Start()
     {
@@ -18,23 +20,29 @@ public class ShipMovement : MonoBehaviour
     void Update()
     {
         Vector2 deltaMouse = new Vector2(
-                             Input.GetAxis("mouseX") * horizontalSensitivity,
-                             Input.GetAxis("mouseY") * verticalSensitivity);
+            Input.GetAxis("mouseX") * mouseXSensitivity,
+            Input.GetAxis("mouseY") * mouseYSensitivity);
 
-        // roll yaw pitch
+        // Update the target direction that the player want to face
+        // Roll Yaw Pitch
         targetRotation.z = Mathf.Clamp(targetRotation.z + deltaMouse.y,-90,90);
         targetRotation.y += deltaMouse.x;
 
+        // TEMP: Set rotation to player target rotation, this should happen overtime
+        // and should roll the ship when the player 'flips' the ship
         transform.rotation = Quaternion.Euler(targetRotation);
     }
 
     private void FixedUpdate()
     {
+        // Create a vector to track all movement that we want to do this frame
         Vector3 movement = new Vector3();
 
         // Update movement in local space and rotate with world rotation
         movement.z = -horizontalSpeed * Input.GetAxis("horizontal");
-        movement.x = verticalSpeed * Input.GetAxis("vertical");
+        movement.x = forwardSpeed * Input.GetAxis("forward");
+        movement.y = verticalSpeed * Input.GetAxis("vertical");
+        // NOTE: This may have to be transformed with the target transform instead of actual transform
         movement = transform.rotation * movement;
 
         transform.position += movement * Time.fixedDeltaTime;
