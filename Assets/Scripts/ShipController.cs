14dbbd92a64b95b1cc38408ca91bb5a84504ba49
Angelo <7332321+antjowie@users.noreply.github.com>
2020-01-06 02:00:03 +0100

    Ship rotation is applied smoothly instead of instant

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -2,59 +2,70 @@
 
 public class ShipController : MonoBehaviour
 {
+    [SerializeField] private float mouseXSensitivity = 0.25f;
+    [SerializeField] private float mouseYSensitivity = 0.5f;
+
+    // All speed values are in seconds. For example, 50 pixels in a second
+    [SerializeField] private float forwardSpeed = 50f;
+    [SerializeField] private float verticalSpeed = 25f;
+    [SerializeField] private float horizontalSpeed = 50f;
+    
+    // TEMP: This should be controlled via a weapon script
+    [SerializeField] private float shootCooldown = 0.1f;
     [SerializeField] private GameObject bulletPrefab = null;
 
-    [SerializeField] private float mouseXSensitivity = 1;
-    [SerializeField] private float mouseYSensitivity = 1;
+    // We interpolate over this transform
+    private Vector3 targetRotation = Vector3.zero;
+    private Vector3 rotationVelocity = Vector3.zero;
+    [SerializeField] private float rotationTime = 0.2f;
+    [SerializeField] private float rotationSpeed = 360f;
+    [SerializeField] private float rollSpeed = 360f;
 
-    [SerializeField] private float forwardSpeed = 50;
-    [SerializeField] private float verticalSpeed = 25;
-    [SerializeField] private float horizontalSpeed = 50;
-
-    [SerializeField] private float rollSpeed = 360;
-    
-    [SerializeField] private float shootCooldown = 0.1f;
-
-    // We keep target rotation in Euler Angles so that we can easily modify it
-    [SerializeField]
-    private Vector3 targetRotation;
     private bool canShoot = true;
     private Rigidbody rb;
+    private Transform cameraTrans;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        cameraTrans = transform.Find("Camera").transform;
+        
         // Hold cursor
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-        UpdateMovement();
+        UpdateRotation();
         UpdateShootState();
-
-        if (Input.GetKey(KeyCode.P))
-            targetRotation = Vector3.zero;
     }
 
-    private void UpdateMovement()
+    private void UpdateRotation()
     {
         // We store the current delta mouse pos in a vector so we can easily use it
         Vector2 deltaMouse = new Vector2(
             Input.GetAxis("mouseX") * mouseXSensitivity,
             Input.GetAxis("mouseY") * mouseYSensitivity);
-
+        float rollAmount = Input.GetAxis("roll") * rollSpeed * Time.deltaTime;
+        
         // Update the target direction that the player want to face
         // Pitch Yaw Roll
-        Vector3 deltaRotation = new Vector3();
+        targetRotation.x += -deltaMouse.y;
+        targetRotation.y += deltaMouse.x;
+        targetRotation.z += rollAmount;
 
-        deltaRotation.x += -deltaMouse.y;
-        deltaRotation.y += deltaMouse.x;
-        deltaRotation.z += Input.GetAxis("roll") * rollSpeed * Time.deltaTime;
+        // Apply rotation to local space and do it over time
+        var rotation = Vector3.SmoothDamp(Vector3.zero,targetRotation,ref rotationVelocity,rotationTime);
+        var nextRot = transform.rotation * Quaternion.Euler(rotation);
+        targetRotation -= rotation;
 
-        transform.Rotate(deltaRotation, Space.Self);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, nextRot, rotationSpeed * Time.deltaTime);
+        
+        // Some debug rays to show intended direction
+        Debug.DrawRay(transform.position, transform.forward * 10);
+        Debug.DrawRay(transform.position, transform.rotation * Quaternion.Euler(targetRotation) * Vector3.forward * 10, Color.green);
     }
-
+    
     private void UpdateShootState()
     {
         // Shoot if player wants to 
