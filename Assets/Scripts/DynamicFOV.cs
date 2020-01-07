53accf6b3a33b6f0db9e01b506f061d2fae27694
Angelo <7332321+antjowie@users.noreply.github.com>
2020-01-07 19:37:16 +0100

    Intermediate: Added camera FOV change based on speed

---
+++ Assets/Scripts/DynamicFOV.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class DynamicFOV : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private float speedThreshold = 50f;
+    [SerializeField] private float speedProgression = 10f;
+    [SerializeField] private float minFOV = 60f;
+    [SerializeField] private float maxFOV = 90f;
+    private Cinemachine.CinemachineVirtualCamera virtualCamera;
+    private float oldZ;
+
+    // For debugging purposes
+    float speed;
+    float FOV;
+
+    void Awake()
+    {
+        virtualCamera = GetComponent<Cinemachine.CinemachineVirtualCamera>();    
+    }
+
+    void Start()
+    {
+        oldZ = playerTransform.position.z;
+    }
+
+    void LateUpdate()
+    {
+        speed = (playerTransform.position - oldPos).magnitude;
+        FOV = Mathf.SmoothStep(minFOV, maxFOV, (speed - speedThreshold) / speedProgression);
+        virtualCamera.m_Lens.FieldOfView = FOV;
+
+        oldPos = playerTransform.position;
+    }
+}
