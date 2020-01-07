f80daaf17631b4f95dd8b5b0d26cf82f60d7b051
Angelo <7332321+antjowie@users.noreply.github.com>
2020-01-07 19:02:02 +0100

    Moved movement and rotation code into fixedUpdate to make sure that movement is in sync with physics

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -12,8 +12,7 @@ public class ShipController : MonoBehaviour
     [SerializeField] private float forwardSpeed = 50f;
     [SerializeField] private float verticalSpeed = 25f;
     [SerializeField] private float horizontalSpeed = 50f;
-    private Vector3 movement;
-
+    
     // We interpolate over this transform
     [Header("Rotation")]
     [SerializeField] private float rotationDamping = 0.2f;
@@ -40,18 +39,16 @@ public class ShipController : MonoBehaviour
 
     void Update()
     {
-        UpdateMovement();
-        UpdateRotation();
         UpdateShootState();
     }
 
     private void UpdateMovement()
     {
-        // Update movement in local space and rotate with world rotation
-        movement += transform.forward * forwardSpeed * Input.GetAxis("forward");
-        movement += transform.right * horizontalSpeed * Input.GetAxis("horizontal");
-        movement += transform.up * verticalSpeed * Input.GetAxis("vertical");
-        movement *= Time.deltaTime;
+        // Update movement based on world space
+        rb.MovePosition(rb.position +
+            transform.forward * forwardSpeed * Input.GetAxis("forward") * Time.fixedDeltaTime +
+            transform.right * horizontalSpeed * Input.GetAxis("horizontal") * Time.fixedDeltaTime +
+            transform.up * verticalSpeed * Input.GetAxis("vertical") * Time.fixedDeltaTime);
     }
 
     private void UpdateRotation()
@@ -60,13 +57,21 @@ public class ShipController : MonoBehaviour
         Vector2 deltaMouse = new Vector2(
             Input.GetAxis("mouseX") * mouseXSensitivity,
             Input.GetAxis("mouseY") * mouseYSensitivity);
-        float rollAmount = Input.GetAxis("roll") * rollCap * Time.deltaTime;
+        float rollAmount = Input.GetAxis("roll") * rollCap * Time.fixedDeltaTime;
         
         // Update the target direction that the player want to face
         // Pitch Yaw Roll
         targetRotation.x += -deltaMouse.y;
         targetRotation.y += deltaMouse.x;
         targetRotation.z += rollAmount;
+
+        // Apply rotation to local space and do it over time
+        Vector3 rotation = Vector3.SmoothDamp(Vector3.zero, targetRotation, ref rotationVelocity, rotationDamping, rotationSpeed, Time.fixedDeltaTime);
+        Quaternion nextRot = transform.rotation * Quaternion.Euler(rotation);
+        targetRotation -= rotation;
+
+        Quaternion oldRot = rb.rotation;
+        rb.MoveRotation(Quaternion.RotateTowards(rb.rotation, nextRot, rotationSpeed * Time.fixedDeltaTime));
 
         // Some debug rays to show intended direction
         Debug.DrawRay(transform.position, transform.forward * 10);
@@ -91,17 +96,7 @@ public class ShipController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement);
-        movement = Vector3.zero;
-
-        // Apply rotation to local space and do it over time
-        Vector3 rotation = Vector3.SmoothDamp(Vector3.zero, targetRotation, ref rotationVelocity, rotationDamping, rotationSpeed, Time.fixedDeltaTime);
-        Quaternion nextRot = transform.rotation * Quaternion.Euler(rotation);
-        targetRotation -= rotation;
-
-        Quaternion oldRot = rb.rotation;
-        rb.MoveRotation(Quaternion.RotateTowards(rb.rotation, nextRot, rotationSpeed * Time.fixedDeltaTime));
-        //targetRotation -= Quaternion.;
-
+        UpdateMovement();
+        UpdateRotation();
     }
 }
