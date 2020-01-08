90f2901373ae08861be46f67fcca920c35ff97e8
Angelo <7332321+antjowie@users.noreply.github.com>
2020-01-08 14:36:49 +0100

    Created a script that changes the FOV of the camera based on the forward speed of a tracked object

--- Assets/Scripts/DynamicFOV.cs
+++ Assets/Scripts/DynamicFOV.cs
@@ -3,12 +3,14 @@
 public class DynamicFOV : MonoBehaviour
 {
     [SerializeField] private Transform playerTransform;
-    [SerializeField] private float speedThreshold = 50f;
-    [SerializeField] private float speedProgression = 10f;
+    [SerializeField] private float speedThreshold = 1f;
+    [SerializeField] private float FOVChangeTime = 1f;
+
     [SerializeField] private float minFOV = 60f;
     [SerializeField] private float maxFOV = 90f;
     private Cinemachine.CinemachineVirtualCamera virtualCamera;
-    private float oldZ;
+    private Vector3 oldPos;
+    private float FOVProgress;
 
     // For debugging purposes
     float speed;
@@ -21,13 +23,24 @@ public class DynamicFOV : MonoBehaviour
 
     void Start()
     {
-        oldZ = playerTransform.position.z;
+        oldPos = playerTransform.position;
     }
 
-    void LateUpdate()
+    void FixedUpdate()
     {
-        speed = (playerTransform.position - oldPos).magnitude;
-        FOV = Mathf.SmoothStep(minFOV, maxFOV, (speed - speedThreshold) / speedProgression);
+        // Calculate forward speed
+        Vector3 forwardMovement = playerTransform.position - oldPos;
+        speed = Vector3.Project(forwardMovement,playerTransform.forward).magnitude;
+        if (Vector3.Dot(forwardMovement, playerTransform.forward) < 0f) // We only want to apply if player moves forward
+            speed = 0f;
+
+        // Calculate new progression value
+        float progress = speed > speedThreshold ? Time.fixedDeltaTime : -Time.fixedDeltaTime;
+        progress /= FOVChangeTime;
+        FOVProgress = Mathf.Clamp(FOVProgress + progress, 0, 1);
+
+        // Apply new FOV
+        FOV = Mathf.SmoothStep(minFOV, maxFOV, FOVProgress);
         virtualCamera.m_Lens.FieldOfView = FOV;
 
         oldPos = playerTransform.position;
