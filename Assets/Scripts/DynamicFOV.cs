e1e001e3d9c0ceec7dcdb10e27b87325a134ef1a
Angelo <7332321+antjowie@users.noreply.github.com>
2020-01-08 15:27:52 +0100

    Added variables to change how fast FOV increases and decreases

--- Assets/Scripts/DynamicFOV.cs
+++ Assets/Scripts/DynamicFOV.cs
@@ -4,17 +4,20 @@ public class DynamicFOV : MonoBehaviour
 {
     [SerializeField] private Transform playerTransform;
     [SerializeField] private float speedThreshold = 1f;
-    [SerializeField] private float FOVChangeTime = 1f;
+    [SerializeField] private float FOVIncreaseRate = 1f;
+    [SerializeField] private float FOVDecreaseRate = 0.5f;
 
     [SerializeField] private float minFOV = 60f;
     [SerializeField] private float maxFOV = 90f;
     private Cinemachine.CinemachineVirtualCamera virtualCamera;
     private Vector3 oldPos;
+    private float oldSpeed;
+    private float maxSpeed;
     private float FOVProgress;
-
-    // For debugging purposes
-    float speed;
-    float FOV;
+    
+    // For debuggin purposes
+    private float speed;
+    private float FOV;
 
     void Awake()
     {
@@ -35,14 +38,19 @@ public class DynamicFOV : MonoBehaviour
             speed = 0f;
 
         // Calculate new progression value
-        float progress = speed > speedThreshold ? Time.fixedDeltaTime : -Time.fixedDeltaTime;
-        progress /= FOVChangeTime;
+        float progress = speed > speedThreshold ? 
+            Time.fixedDeltaTime * FOVIncreaseRate: 
+            -Time.fixedDeltaTime * FOVDecreaseRate;
         FOVProgress = Mathf.Clamp(FOVProgress + progress, 0, 1);
+        
 
         // Apply new FOV
         FOV = Mathf.SmoothStep(minFOV, maxFOV, FOVProgress);
         virtualCamera.m_Lens.FieldOfView = FOV;
 
         oldPos = playerTransform.position;
+        oldSpeed = speed;
+        if (speed > maxSpeed)
+            maxSpeed = speed;
     }
 }
